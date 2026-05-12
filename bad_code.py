884547a929a2753ab605dcf86f283bac6eea5897
NEHA B R <nehabr47@gmail.com>
2026-05-12 11:38:28 +0530

    Create bad_code.py

---
+++ bad_code.py
@@ -0,0 +1,35 @@
+import random
+
+password = "12345"
+
+a = 10
+b = 0
+
+try:
+    print(a / b)
+except:
+    pass
+
+for i in range(5):
+    print("Hello")
+
+for i in range(5):
+    print("Hello")
+
+unused_var = 100
+
+x = 5
+y = 10
+z = x + y
+
+count = 0
+while count >= 0:
+    print(count)
+    count += 1
+    if count == 3:
+        break
+
+numbers = [1, 2, 3, 4, 5]
+random.shuffle(numbers)
+
+print(numbers)
